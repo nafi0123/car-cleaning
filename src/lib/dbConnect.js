cf2556ca844ca1a7ac1ad473440a7b22799954e7
Nafi Mahmud Bukhari <nafi.cse0123@gmail.com>
2026-01-30 22:45:13 +0600

    db connect sucessfully and api hit correctly

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,22 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.NEXT_MONGO_URI;
+const dbName = process.env.NEXT_MONGO_NAME;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const dbConnect = async (collectionName) => {
+  try {
+    const db = client.db(dbName);
+    console.log("MongoDB connected!");
+    return db.collection(collectionName);
+  } catch (e) {
+    console.log(e);
+  }
+};
