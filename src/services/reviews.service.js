4963a70e1816e6d4cca8172049fbee89b1074e21
nafi0123 <nafi.cse0123@gmail.com>
2026-02-04 08:00:12 -0800

    reviews services cache in

--- src/services/reviews.service.js
+++ src/services/reviews.service.js
@@ -1,6 +1,6 @@
 "use server";
 
-
+import { revalidateTag } from "next/cache";
 
 export const createReview = async (data) => {
   const res = await fetch(
@@ -18,7 +18,8 @@ export const createReview = async (data) => {
     throw new Error("Failed to create review");
   }
 
-
+  //   revalidatePath("/reviews");
+  revalidateTag("reviews");
 
   return res.json();
 };
@@ -31,10 +32,20 @@ export const getAllReviews = async (searchParams) => {
     `${
       process.env.NEXT_AUTH_URL || "http://localhost:3000"
     }/api/reviews?${getParams}`,
-
+    {
+      cache: "force-cache", // default: no-store
+      next: {
+        tags: ["reviews"],
+        revalidate: 60, // ISR = Incremental Static Regeneration
+      },
+    }
   );
 
-
+  //   await new Promise((resolve) =>
+  //     setTimeout(() => {
+  //       resolve();
+  //     }, 3000)
+  //   );
 
   const data = await res.json();
   return data;
