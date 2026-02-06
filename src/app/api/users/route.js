6b71f4658b72d47d9b0ae151bf40974555c1b3e3
nafi0123 <nafi.cse0123@gmail.com>
2026-02-06 19:17:02 +0600

    user.service.js + user route.js create

---
+++ src/app/api/users/route.js
@@ -0,0 +1,26 @@
+import { dbConnect } from "@/lib/dbConnect";
+import bcrypt from "bcrypt";
+
+const users = await dbConnect("users");
+
+export async function POST(request) {
+  const reqBody = await request.json();
+
+  const isUserExist = await users.findOne({ email: reqBody.email });
+  if (isUserExist) {
+    return Response.json({
+      status: 400,
+      message: "User already exists",
+    });
+  }
+
+  const passwordHash = await bcrypt.hash(reqBody.password, 10);
+
+  const newUser = await users.insertOne({ ...reqBody, password: passwordHash });
+
+  return Response.json({
+    status: 201,
+    message: "User created successfully",
+    data: newUser,
+  });
+}
--- src/lib/providers/index.js
+++ src/lib/providers/index.js
@@ -1,12 +1,16 @@
 import BookingsContextProvider from "@/context/booking.context";
 import UserContextProvider from "@/context/user.context";
+import { SessionProvider } from "next-auth/react";
+
 import React from "react";
 
 const Providers = ({ children }) => {
   return (
-    <UserContextProvider>
-      <BookingsContextProvider>{children}</BookingsContextProvider>
-    </UserContextProvider>
+    <SessionProvider>
+      <UserContextProvider>
+        <BookingsContextProvider>{children}</BookingsContextProvider>
+      </UserContextProvider>
+    </SessionProvider>
   );
 };
 
